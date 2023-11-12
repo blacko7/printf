1db05c9f9f2a7b22d88a26b33ebbdecf21d4c526
KwameElorm <elormh@ymail.com>
2023-11-12 08:56:45 -0800

    5. My weakness is wearing too much leopard print

--- main.h
+++ main.h
@@ -1,10 +1,23 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stadarg.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int printf_char(va_list val)
-
-
+int printf_char(va_list val);
+int prinf_string(va_list val);
+int _strlenc(const char *str);
+int _strlen(char *str);
+int print_37(void);
+int print_d(va_list args);
+int print_i(va_list args);
+int print_bin(va_list val);
+int print_unsigned(va_list args);
+int print_oct(va_list val);
+int print_hex(va_list val);
+int print_HEX(va_list val);
+int print_exc_string(va_list val);
 
 #endif
